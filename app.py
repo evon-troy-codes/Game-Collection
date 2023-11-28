73a036802d40e02ada9a5f7b97e235fed86521b0
Manuel Guerra <manuel.guerra1309@gmail.com>
2023-11-28 13:19:19 -0500

    added login functionality with sql and flask_login

--- app.py
+++ app.py
@@ -1,59 +1,37 @@
 from flask import Flask, request, jsonify, make_response, render_template, redirect, url_for, session, flash
 from views import views as views_blueprint
+from models import db, Users, Games
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
+from flask_login import LoginManager
 
 # Create the application instance
 app = Flask(__name__)
-app.register_blueprint(views_blueprint, url_prefix="/")
-app.secret_key = 'testing_key'
-
-
-# # Create the API
-api = Api(app)
 
 # Create the database instance
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:yourpassword!Group@localhost/gamesdatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysqlt3!Group@localhost/gamesdatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# sqlalchemy instance
-db = SQLAlchemy(app)
+# Initialize flask with db
+db.init_app(app)
+
+# # Create the API
+# api = Api(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'views.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+
+app.register_blueprint(views_blueprint, url_prefix='/')
+app.secret_key = 'fdsagasggsdf gsdfdas'
 
 
-# Create the database model
-class Users(db.Model):
-    UserID = db.Column(db.Integer, primary_key=True)
-    Password = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(75), nullable=False)
-    DOB = db.Column(db.Date, default=None)
-    firstName = db.Column(db.String(40))
-    lastName = db.Column(db.String(40))
-
-class Games(db.Model):
-    GameID = db.Column(db.Integer, primary_key=True)
-    Genre = db.Column(db.String(100))
-    MetacriticRating = db.Column(db.Integer)
-    ESRB = db.Column(db.String(5))
-    Title = db.Column(db.String(100))
-    Description = db.Column(db.Text)
-    Developer = db.Column(db.String(100))
-    ReleaseDate = db.Column(db.Date)
-    AverageRating = db.Column(db.Float(precision=2))
-
-
-if __name__ == "__main__":
-    # db.create_all()
-    # app.run(debug=True)
-
-    # to test that database is working
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        first_user = Users.query.first()
-
-        if first_user:
-            print(f"User ID: {first_user.UserID}")
-            print(f"Email: {first_user.email}")
-            print(f"Date of Birth: {first_user.DOB}")
-        else:
-            print("No users found in the users table.")
+        app.run(debug=True)
