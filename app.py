116475c7fa040047aac32770fa88a26cde0cb49b
Evon Troy Alexander <evont.alexander@gmail.com>
2023-11-30 13:30:15 -0500

    Amended the DB Model

--- app.py
+++ app.py
@@ -10,26 +10,61 @@ from flask_login import LoginManager
 app = Flask(__name__)
 
 # Create the database instance
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysqlt3!Group@localhost/gamesdatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:ealexan2@localhost/gamesdatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize flask with db
-db.init_app(app)
+db = SQLAlchemy(app)
 
-# # Create the API
-# api = Api(app)
+
+class Game(db.Model):
+    game_id = db.Column(db.Integer, primary_key=True)
+    genre = db.Column(db.String(100))
+    MetacriticRating = db.Column(db.Integer)
+    esrb = db.Column(db.String(5))
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    developer = db.Column(db.String(100))
+    release_date = db.Column(db.Date)
+    AverageRating = db.Column(db.Float(precision=2))
+
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(75), nullable=False)
+    dob = db.Column(db.Date)
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(40))
+
+
+class GameCollection(db.Model):
+    collection_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+
+
+class GameCollectionItems(db.Model):
+    collection_items_id = db.Column(db.Integer, primary_key=True)
+    game_collection_id = db.Column(db.Integer, db.ForeignKey('gamecollection.collection_id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('game.game_id'))
+    status = db.Column(db.String(20))
+    UserRating = db.Column(db.Integer)
+    favorite_games = db.Column(db.Boolean)
+    total_play_time = db.Column(db.Integer)
+
 
 login_manager = LoginManager()
 login_manager.login_view = 'views.login'
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(id):
     return Users.query.get(int(id))
 
+
 app.register_blueprint(views_blueprint, url_prefix='/')
 app.secret_key = 'fdsagasggsdf gsdfdas'
-
 
 if __name__ == '__main__':
     with app.app_context():
