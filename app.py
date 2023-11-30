93c44928d824722264544349b3adf7c6679b13c2
Evon Troy Alexander <evont.alexander@gmail.com>
2023-11-30 13:12:57 -0500

    Amended the DB Model

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, make_response, render_template, redir
 from views import views as views_blueprint
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-import mysql.connector
 
 # Create the application instance
 app = Flask(__name__)
@@ -10,50 +9,53 @@ app.register_blueprint(views_blueprint, url_prefix="/")
 app.secret_key = 'testing_key'
 
 
-# # Create the API
+# Create the API
 api = Api(app)
 
 # Create the database instance
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:yourpassword!Group@localhost/gamesdatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:ealexan2@localhost/gamesdatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # sqlalchemy instance
 db = SQLAlchemy(app)
 
-
 # Create the database model
-class Users(db.Model):
-    UserID = db.Column(db.Integer, primary_key=True)
-    Password = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(75), nullable=False)
-    DOB = db.Column(db.Date, default=None)
-    firstName = db.Column(db.String(40))
-    lastName = db.Column(db.String(40))
-
-class Games(db.Model):
-    GameID = db.Column(db.Integer, primary_key=True)
-    Genre = db.Column(db.String(100))
+class Game(db.Model):
+    game_id = db.Column(db.Integer, primary_key=True)
+    genre = db.Column(db.String(100))
     MetacriticRating = db.Column(db.Integer)
-    ESRB = db.Column(db.String(5))
-    Title = db.Column(db.String(100))
-    Description = db.Column(db.Text)
-    Developer = db.Column(db.String(100))
-    ReleaseDate = db.Column(db.Date)
+    esrb = db.Column(db.String(5))
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    developer = db.Column(db.String(100))
+    release_date = db.Column(db.Date)
     AverageRating = db.Column(db.Float(precision=2))
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(75), nullable=False)
+    dob = db.Column(db.Date)
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(40))
+
+class GameCollection(db.Model):
+    collection_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+
+class GameCollectionItems(db.Model):
+    collection_items_id = db.Column(db.Integer, primary_key=True)
+    game_collection_id = db.Column(db.Integer, db.ForeignKey('gamecollection.collection_id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('game.game_id'))
+    status = db.Column(db.String(20))
+    UserRating = db.Column(db.Integer)
+    favorite_games = db.Column(db.Boolean)
+    total_play_time = db.Column(db.Integer)
+
+
 
 
 if __name__ == "__main__":
-    # db.create_all()
-    # app.run(debug=True)
+    db.create_all()
+    app.run(debug=True)
 
-    # to test that database is working
-    with app.app_context():
-        db.create_all()
-        first_user = Users.query.first()
-
-        if first_user:
-            print(f"User ID: {first_user.UserID}")
-            print(f"Email: {first_user.email}")
-            print(f"Date of Birth: {first_user.DOB}")
-        else:
-            print("No users found in the users table.")
